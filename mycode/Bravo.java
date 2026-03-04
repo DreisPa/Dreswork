dd37bedc109facc6161b664a9e1721f98015e7a8
DreisPa <132481982+DreisPa@users.noreply.github.com>
2026-03-04 05:00:06 +0500

    Update Bravo.java

--- mycode/Bravo.java
+++ mycode/Bravo.java
@@ -1,1 +1,13 @@
-
+package mycode;
+class Bravo extends Alpha{
+  String name;
+  coid show(){
+    System.out.println("Унаследованное имя из Alpha - "+super.name);
+    System.out.println("Из класса Bravo - "+name);
+  }
+  Bravo(String a,String b){
+    super();
+    super.name = a;
+    name = b;
+  }
+}
